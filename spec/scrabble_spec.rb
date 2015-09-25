b793ab43601044962a12945c8dd33dddab0159a6
Jason Pilz <jasonpilz@gmail.com>
2015-09-24 19:26:06 -0600

    Implemented Scrabble funtionality

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -1,10 +1,13 @@
-require './lib/scrabble'
+require_relative '../lib/scrabble'
+require 'rspec'
+require 'minitest/autorun'
+require 'minitest/pride'
 
 describe Scrabble do
-  describe "#score" do
-    xit "scores a single letter" do
-      expect( Scrabble.new.score("a") ).to eq 1
-      expect( Scrabble.new.score("f") ).to eq 4
-    end
-  end
+  # describe "#score" do
+  #   it "scores a single letter" do
+  #     expect(Scrabble.new.score("a")).to eql(1)
+  #     expect(Scrabble.new.score("f")).to eql(4)
+  #   end
+  # end
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -6,8 +6,30 @@ require 'pry'
 
 class ScrabbleTest < Minitest::Test
   def test_it_can_score_a_single_letter
-    skip
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
   end
+
+  def test_it_can_score_two_letters
+    assert_equal 5, Scrabble.new.score("af")
+    assert_equal 2, Scrabble.new.score("to")
+  end
+
+  def test_it_can_score_all_letters
+    assert_equal 87, Scrabble.new.score("abcdefghijklmnopqrstuvwxyz")
+  end
+
+  def test_can_score_a_letter_insensitive_to_case
+    assert_equal 4, Scrabble.new.score('f')
+    assert_equal 4, Scrabble.new.score('F')
+  end
+
+  def test_score_for_empty_word_returns_0
+    assert_equal 0, Scrabble.new.score("")
+  end
+
+  def test_score_for_nil_returns_0
+    assert_equal 0, Scrabble.new.score(nil)
+  end
+
 end
